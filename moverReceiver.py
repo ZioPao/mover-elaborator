c9e7239dd6ae983b920c502696ebb11ab66e031c
ZioPao <picchetti1998@gmail.com>
2021-05-21 17:36:18 +0200

    had to disable BT for now... No idea why it doesn't work anymore

--- moverReceiver.py
+++ moverReceiver.py
@@ -123,19 +123,23 @@ class MoverReceiver:
 
     def read_decode_data(self):
         try:
-            ser_bytes_data_line = self.main_mover.readline()
-            decoded_bytes_data_line = ser_bytes_data_line.decode()
-            print(decoded_bytes_data_line)
-            regex_search = re.findall('m (\S*),(\S*),(\S*),(\S*),(\S*),(\S*) s (\S*),(\S*),(\S*),(\S*),(\S*),(\S*) e', decoded_bytes_data_line)[0]
+            ser_bytes_data_line_main = self.main_mover.readline()
+            ser_bytes_data_line_slave = self.slave_mover.readline()
+
+            ser_bytes_data_line_main = ser_bytes_data_line_main.decode()
+            ser_bytes_data_line_slave = ser_bytes_data_line_slave.decode()
+
+            regex_search_main = re.findall('(\S*),(\S*),(\S*),', ser_bytes_data_line_main)[0]
+            regex_search_slave = re.findall('(\S*),(\S*),(\S*),', ser_bytes_data_line_slave)[0]
+
             #regex_search = re.findall("(\S*),(\S*),(\S*),(\S*),(\S*),(\S*)", decoded_bytes_data_line[:-2])[0]
 
-            m_raw_x = float(regex_search[0]) / DATA_DIVIDER
-            m_raw_y = float(regex_search[1]) / DATA_DIVIDER
-            m_raw_z = float(regex_search[2]) / DATA_DIVIDER
-
-            s_raw_x = float(regex_search[6]) / DATA_DIVIDER
-            s_raw_y = float(regex_search[7]) / DATA_DIVIDER
-            s_raw_z = float(regex_search[8]) / DATA_DIVIDER
+            m_raw_x = float(regex_search_main[0]) / DATA_DIVIDER
+            m_raw_y = float(regex_search_main[1]) / DATA_DIVIDER
+            m_raw_z = float(regex_search_main[2]) / DATA_DIVIDER
+            s_raw_x = float(regex_search_slave[0]) / DATA_DIVIDER
+            s_raw_y = float(regex_search_slave[1]) / DATA_DIVIDER
+            s_raw_z = float(regex_search_slave[2]) / DATA_DIVIDER
 
             self.acc_values.append([m_raw_x, m_raw_y, m_raw_z])
             self.acc_values.append([s_raw_x, s_raw_y, s_raw_z])
